0f3b6dd1d0830cae5e5c206e151b1c8e728455f8
Saeed Alshammari <saeed29994@gmail.com>
2025-05-28 00:39:22 +0300

    Update files

--- src/components/MiningCard.tsx
+++ src/components/MiningCard.tsx
@@ -94,7 +94,7 @@ const MiningCard: React.FC<MiningCardProps> = ({ plan, onClaim }) => {
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = seconds % 60;
-    return \`\${h}h \${m}m \${s}s\`;
+    return `${h}h ${m}m ${s}s`;
   };
 
   return (
